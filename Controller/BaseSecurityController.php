c6cf408c2df4eaeef2aff48991ace8c6f3d1149b
Jannik Zschiesche <hello@apfelbox.net>
2013-09-13 11:46:39 +0200

    Added base security controller

---
+++ Controller/BaseSecurityController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace OAGM\BaseBundle\Controller;
+
+use Symfony\Component\Security\Core\SecurityContext;
+
+abstract class BaseSecurityController extends BaseController
+{
+    /**
+     * Returns the template name of the login controller
+     *
+     * @return mixed
+     */
+    protected function getTemplate()
+    {
+        $namespaceParts = explode("\\", trim(get_class($this), "\\"));
+        $bundle = $namespaceParts[0] . str_replace("Bundle", "", $namespaceParts[1]);
+        return "@{$bundle}/Security/login.html.twig";
+    }
+
+
+
+    /**
+     * Handles the login action
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function loginAction ()
+    {
+        $request = $this->getRequest();
+        $session = $request->getSession();
+
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                SecurityContext::AUTHENTICATION_ERROR
+            );
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+
+        return $this->render(
+            $this->getTemplate(),
+            array(
+                'username' => $session->get(SecurityContext::LAST_USERNAME),
+                'error'    => $error,
+            )
+        );
+    }
+}
