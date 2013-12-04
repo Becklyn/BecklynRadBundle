9b76867f6d6ff19af122975665a72c76c1b5ccd6
Jannik Zschiesche <hello@apfelbox.net>
2013-12-04 17:20:59 +0100

    Added SizedPathService with the simplified version
    For images with different sizes.

---
+++ Service/SimpleSizedPathService.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Becklyn\RadBundle\Service;
+
+use Becklyn\RadBundle\Entity\IdEntity;
+use Becklyn\RadBundle\Helper\ImageHandler;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class SimpleSizedPathService extends SizedPathService
+{
+    /**
+     * @var string
+     */
+    private $imageType;
+
+
+    /**
+     * @var string
+     */
+    private $relativePath;
+
+
+    /**
+     * @var \Symfony\Component\HttpFoundation\Request
+     */
+    private $hashPrefix;
+
+
+
+    /**
+     * Constructs a new simple sized path service
+     *
+     * @param \Symfony\Component\HttpKernel\KernelInterface $kernel
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param string $relativePath the relative path from the web dir
+     * @param string $imageType the image type, on of the ImageHandler::IMAGE_TYPE_* constants
+     * @param string|null $hashPrefix prefix for file name hashing. No hashing is applied to the filename, if $hashPrefix is null
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function __construct (KernelInterface $kernel, Request $request, $relativePath, $imageType, $hashPrefix = null)
+    {
+        parent::__construct($kernel, $request);
+
+        if (!ImageHandler::isSupportedImageType($imageType))
+        {
+            throw new \InvalidArgumentException("Image type not supported: {$imageType}. Supported image types are: " . implode(", ", ImageHandler::getSupportedImageTypes()));
+        }
+
+        $this->imageType     = $imageType;
+        $this->hashPrefix    = $hashPrefix;
+        $this->relativePath  = trim($relativePath, "/");
+    }
+
+
+
+    /**
+     * Returns the path to the storage
+     *
+     * @param string $size
+     *
+     * @return string
+     */
+    protected function getStoragePath ($size)
+    {
+        return "/{$this->relativePath}/{$size}/";
+    }
+
+
+
+    /**
+     * Generates the file name
+     *
+     * @param IdEntity $entity
+     * @param string $size
+     *
+     * @return string
+     */
+    protected function getFileName (IdEntity $entity, $size)
+    {
+        if (is_null($this->hashPrefix))
+        {
+            return "{$size}-{$entity->getId()}.{$this->imageType}";
+        }
+
+        return md5("{$this->hashPrefix}-{$entity->getId()}-{$size}") . ".{$this->imageType}";
+    }
+}
---
+++ Service/SizedPathService.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Becklyn\RadBundle\Service;
+
+use Becklyn\RadBundle\Entity\IdEntity;
+
+/**
+ * Implements a default path service, which eases the default use case of a file path handler
+ *
+ * @package Becklyn\RadBundle\Service
+ */
+abstract class SizedPathService extends AbstractPathService
+{
+    /**
+     * Returns the web server path of the file
+     *
+     * @param IdEntity $entity
+     * @param string $size
+     *
+     * @return string
+     */
+    public function getWebServerPath (IdEntity $entity, $size)
+    {
+        $flag = "";
+
+        if ($this->hasFile($entity, $size))
+        {
+            $flag = "?v=" . filemtime($this->getFileSystemPath($entity, $size));
+        }
+
+        return "{$this->webServerRoot()}{$this->getStoragePath($size)}{$this->getFilename($entity, $size)}{$flag}";
+    }
+
+
+
+    /**
+     * Returns the file system path of the file
+     *
+     * @param IdEntity $entity
+     * @param string $size
+     *
+     * @return string
+     */
+    public function getFileSystemPath (IdEntity $entity, $size)
+    {
+        return "{$this->fileSystemRoot()}{$this->getStoragePath($size)}{$this->getFilename($entity, $size)}";
+    }
+
+
+
+    /**
+     * Returns whether the file exists
+     *
+     * @param IdEntity $entity
+     * @param string $size
+     *
+     * @return bool
+     */
+    public function hasFile (IdEntity $entity, $size)
+    {
+        if (is_null($entity->getId()))
+        {
+            return false;
+        }
+
+        return is_file($this->getFileSystemPath($entity, $size));
+    }
+
+
+
+    /**
+     * Returns the path to the storage
+     *
+     * @param string $size
+     *
+     * @return string
+     */
+    abstract protected function getStoragePath ($size);
+
+
+
+    /**
+     * Returns the file name for the entity
+     *
+     * @param IdEntity $entity
+     * @param string $size
+     *
+     * @return string
+     */
+     abstract protected function getFilename (IdEntity $entity, $size);
+}
