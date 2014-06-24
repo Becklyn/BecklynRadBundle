da42f1472f4833e8145b2173e8eeb763b19046fb
Jannik Zschiesche <jz@becklyn.com>
2014-06-24 11:33:22 +0200

    Added support for cancelUrl

--- Type/Extension/GenericTypeExtension.php
+++ Type/Extension/GenericTypeExtension.php
@@ -8,7 +8,7 @@ use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class HelpMessageTypeExtension extends AbstractTypeExtension
+class GenericTypeExtension extends AbstractTypeExtension
 {
     /**
      * {@inheritdoc}
@@ -16,6 +16,7 @@ class HelpMessageTypeExtension extends AbstractTypeExtension
     public function buildForm (FormBuilderInterface $builder, array $options)
     {
         $builder->setAttribute('help', $options['help']);
+        $builder->setAttribute('cancelUrl', $options['cancelUrl']);
     }
 
 
@@ -25,7 +26,7 @@ class HelpMessageTypeExtension extends AbstractTypeExtension
      */
     public function buildView (FormView $view, FormInterface $form, array $options)
     {
-        $view->vars['help'] = $form->getConfig()->getAttribute('help');
+        $view->vars['cancelUrl'] = $form->getConfig()->getAttribute('cancelUrl');
     }
 
 
@@ -35,9 +36,10 @@ class HelpMessageTypeExtension extends AbstractTypeExtension
      */
     public function setDefaultOptions (OptionsResolverInterface $resolver)
     {
-        $resolver->setDefaults(array(
-            'help' => null,
-        ));
+        $resolver->setDefaults([
+            'help'      => null,
+            'cancelUrl' => null,
+        ]);
     }
 
 
