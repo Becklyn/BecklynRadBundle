b11c2025684311e3f79ee9d3bc4ac281ead1f0b4
Jannik Zschiesche <jz@becklyn.com>
2015-05-07 19:17:17 +0200

    Removed all controller methods that are already supported in the symfony base controller

--- Controller/BaseController.php
+++ Controller/BaseController.php
@@ -11,69 +11,6 @@ use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 class BaseController extends Controller
 {
     /**
-     * Returns a redirection to the given route
-     *
-     * @param string $route
-     * @param array $parameters
-     *
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse
-     */
-    protected function redirectPath ($route, array $parameters = array())
-    {
-        return $this->redirect(
-            $this->generateUrl($route, $parameters)
-        );
-    }
-
-
-
-    /**
-     * Adds a flash message
-     *
-     * @deprecated use {@link self::addFlashMessage()} instead
-     *
-     * @param string $title
-     * @param string $message
-     * @param string $type
-     *
-     * @throws \InvalidArgumentException
-     */
-    protected function addFlash ($message, $title = null, $type = "warning")
-    {
-        $this->addFlashMessage($message, $type, "admin-general", $title);
-    }
-
-
-
-    /**
-     * Adds a flash message
-     *
-     * @param string $message
-     * @param string $type
-     * @param string $flashBagName
-     * @param null|string $title
-     *
-     * @throws \InvalidArgumentException
-     */
-    protected function addFlashMessage ($message, $type = "success", $flashBagName = "messages", $title = null)
-    {
-        $allowedTypes = ["warning", "error", "success", "info", "danger"];
-
-        if (!in_array($type, $allowedTypes, true))
-        {
-            throw new \InvalidArgumentException("Unknown flash type: {$type}");
-        }
-
-        $this->get('session')->getFlashBag()->add($flashBagName, [
-                "message" => $message,
-                "title"   => $title,
-                "type"    => $type
-            ]);
-    }
-
-
-
-    /**
      * Returns the form error mapping for easier marking of form errors in
      * javascript handler code.
      *
@@ -116,21 +53,6 @@ class BaseController extends Controller
 
 
     /**
-     * Wrapper for isGranted by the security context
-     *
-     * @param mixed      $attributes
-     * @param mixed|null $object
-     *
-     * @return bool
-     */
-    protected function isGranted ($attributes, $object = null)
-    {
-        return $this->get("security.context")->isGranted($attributes, $object = null);
-    }
-
-
-
-    /**
      * Authenticates as the given user.
      *
      * @param UserInterface $user       the user to authenticate
@@ -138,9 +60,9 @@ class BaseController extends Controller
      */
     protected function authenticateUser (UserInterface $user, $firewall = "secured_area")
     {
-        $token = new UsernamePasswordToken($user, null, "profile_area", $user->getRoles());
+        $token = new UsernamePasswordToken($user, null, $firewall, $user->getRoles());
         $this->get("security.context")->setToken($token);
-        $this->get("session")->set('_security_secured_area', serialize($token));
+        $this->get("session")->set("_security_{$firewall}", serialize($token));
     }
 
 
