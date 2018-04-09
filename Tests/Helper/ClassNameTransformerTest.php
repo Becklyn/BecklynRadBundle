575fc33126402ee979a601b47e69cbf32304f12a
Jannik Zschiesche <jz@becklyn.com>
2018-04-09 16:41:21 +0200

    Update tests + fix regex

--- Tests/Helper/ClassNameTransformerTest.php
+++ Tests/Helper/ClassNameTransformerTest.php
@@ -1,23 +1,28 @@
 <?php
 
-namespace Becklyn\RadBundle\Tests\Helper;
+namespace Tests\Becklyn\RadBundle\Helper;
 
 use Becklyn\RadBundle\Helper\ClassNameTransformer;
+use PHPUnit\Framework\TestCase;
 
 
 /**
  *
  */
-class ClassNameTransformerTest extends \PHPUnit_Framework_TestCase
+class ClassNameTransformerTest extends TestCase
 {
     public function getData ()
     {
         return [
-            ["TestBundle\\Model\\TestModel", "TestBundle\\Entity\\Test", "simple case"],
-            ["ABC\\TestBundle\\Model\\TestModel", "ABC\\TestBundle\\Entity\\Test", "correct with nesting"],
-            ["TestBundle\\Model\\Sub\\TestModel", "TestBundle\\Entity\\Sub\\Test", "support sub-namespace"],
-            ["TestBundle\\Model\\Sub\\Sub2\\TestModel", "TestBundle\\Entity\\Sub\\Sub2\\Test", "support 2x sub-namespace"],
-            ["Test\\Model\\TestModel", "Test\\Model\\TestModel", "invalid: no bundle"],
+            ["TestBundle\\Model\\TestModel", "TestBundle\\Entity\\Test", "bundle: simple case"],
+            ["ABC\\TestBundle\\Model\\TestModel", "ABC\\TestBundle\\Entity\\Test", "bundle: correct with nesting"],
+            ["TestBundle\\Model\\Sub\\TestModel", "TestBundle\\Entity\\Sub\\Test", "bundle: support sub-namespace"],
+            ["TestBundle\\Model\\Sub\\Sub2\\TestModel", "TestBundle\\Entity\\Sub\\Sub2\\Test", "bundle: support 2x sub-namespace"],
+
+            ["App\\Model\\TestModel", "App\\Entity\\Test", "bundle-less: simple case"],
+            ["App\\Model\\Sub\\Sub2\\TestModel", "App\\Entity\\Sub\\Sub2\\Test", "bundle-less: correct with nesting"],
+            ["App\\Model\\Sub\\TestModel", "App\\Entity\\Sub\\Test", "bundle-less: support sub-namespace"],
+
             ["TestBundle\\Model\\Test", "TestBundle\\Model\\Test", "invalid: no model"],
             ["TestBundle\\Model\\Model", "TestBundle\\Model\\Model", "invalid edge case: empty entity name"],
         ];
