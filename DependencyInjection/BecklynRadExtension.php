7a1c7a1609f755636486c8cb5099147e3f205c0a
Jannik Zschiesche <jz@becklyn.com>
2018-04-26 18:14:34 +0200

    CS + remove obsolete comment

--- DependencyInjection/BecklynRadExtension.php
+++ DependencyInjection/BecklynRadExtension.php
@@ -7,19 +7,15 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
-/**
- * This is the class that loads and manages your bundle configuration
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
- */
+
 class BecklynRadExtension extends Extension
 {
     /**
      * {@inheritDoc}
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load (array $configs, ContainerBuilder $container)
     {
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
     }
 }
