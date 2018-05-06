4e1ab6f82167fc46629c4a31f9080351f4aacfd7
Jannik Zschiesche <jz@becklyn.com>
2018-05-06 16:10:41 +0200

    Add path helper

---
+++ tests/Path/PathHelperTest.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+namespace Becklyn\RadBundle\tests\Path;
+
+use Becklyn\RadBundle\Path\PathHelper;
+use PHPUnit\Framework\TestCase;
+
+
+class PathHelperTest extends TestCase
+{
+    /**
+     * @return array
+     */
+    public function providerVariations ()
+    {
+        return [
+            [["a", "b", "c"], "a/b/c"],
+            [["/a/", "/b/", "/c/"], "/a/b/c/"],
+
+            // keep leading slash untouched
+            [["a", "b", "c"], "a/b/c"],
+            [["/a", "b", "c"], "/a/b/c"],
+
+            // keep trailing slash untouched
+            [["a", "b", "c"], "a/b/c"],
+            [["a", "b", "c/"], "a/b/c/"],
+        ];
+    }
+
+
+    /**
+     * @dataProvider providerVariations
+     */
+    public function testVariations (array $segments, string $expected)
+    {
+        $actual = PathHelper::join(...$segments);
+        self::assertSame($expected, $actual);
+    }
+}
