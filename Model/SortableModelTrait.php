8b967778445bceb233573e1c5b0e8990228d54c2
Jannik Zschiesche <jz@becklyn.com>
2018-07-06 00:28:47 +0200

    Add sortable model trait to easy implementation of the applySortOrderMapping method

---
+++ Model/SortableModelTrait.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace Becklyn\RadBundle\Model;
+
+/**
+ * Trait for doctrine models that use a sortable handler
+ */
+trait SortableModelTrait
+{
+    /**
+     * Applies the sort order mapping as provided in the parameters.
+     *
+     * The model should wrap this method and use type hints on the $where parameter entries.
+     *
+     * @param array $sortMapping
+     * @param array $where
+     *
+     * @return bool
+     */
+    protected function applySortOrderMapping (array $sortMapping, array $where) : bool
+    {
+        if ($this->sortableHandler->applySorting($sortMapping, $where))
+        {
+            $this->flush();
+            return true;
+        }
+
+        return false;
+    }
+}
