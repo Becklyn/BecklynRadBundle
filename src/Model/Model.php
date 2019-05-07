af0663fa92fd2be859b9a8430e4b84b51e0a677f
Jannik Zschiesche <jz@becklyn.com>
2019-05-07 15:35:06 +0200

    Add base model

---
+++ src/Model/Model.php
@@ -0,0 +1,71 @@
+<?php declare(strict_types=1);
+
+namespace Becklyn\RadBundle\Model;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * Base class for all models.
+ */
+abstract class Model
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $entityManager;
+
+
+    /**
+     * @param RegistryInterface $registry
+     */
+    public function __construct (RegistryInterface $registry)
+    {
+        $this->entityManager = $registry->getEntityManager();
+    }
+
+
+    /**
+     * Marks the entity for adding.
+     *
+     * @param object $entity
+     */
+    public function add (object $entity) : void
+    {
+        $this->entityManager->persist($entity);
+    }
+
+
+    /**
+     * Updates the given entity.
+     *
+     * @param object $entity
+     */
+    public function update (object $entity) : void
+    {
+        if (\method_exists($entity, 'markAsModified'))
+        {
+            $entity->markAsModified();
+        }
+    }
+
+
+    /**
+     * Marks the entity for removal.
+     *
+     * @param object $entity
+     */
+    public function remove (object $entity)
+    {
+        $this->entityManager->remove($entity);
+    }
+
+
+    /**
+     * Flushes the entity changes to the database.
+     */
+    public function flush () : void
+    {
+        $this->entityManager->flush();
+    }
+}
