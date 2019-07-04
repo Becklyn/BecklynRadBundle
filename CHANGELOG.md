24a713f8948f1fb8378e5d2852809160890c8f15
Jannik Zschiesche <jz@becklyn.com>
2019-07-04 12:53:32 +0200

    Re-add row_attr

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,3 +11,4 @@ Changelog for 6.0
 *   Added `ChoicePlaceholderFormExtension`. Symfony removes "placeholder" attributes for certain configurations. 
     In these cases, the placeholder is re-added as `data-placeholder` attribute (if not already set).
 *   Added a default form theme in `@BecklynRad/form/theme.html.twig`.
+*   Added `row_attr` option for forms, with which you can apply attributes the row container in form themes.
---
+++ src/Form/Extension/RowAttrFormExtension.php
@@ -0,0 +1,49 @@
+<?php declare(strict_types=1);
+
+namespace Becklyn\RadBundle\Form\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RowAttrFormExtension extends AbstractTypeExtension
+{
+    /**
+     * @inheritDoc
+     */
+    public function buildForm (FormBuilderInterface $builder, array $options) : void
+    {
+        $builder->setAttribute("row_attr", $options["row_attr"]);
+    }
+    /**
+     * @inheritDoc
+     */
+    public function buildView (FormView $view, FormInterface $form, array $options) : void
+    {
+        $view->vars["row_attr"] = $form->getConfig()->getAttribute("row_attr");
+    }
+    /**
+     * @inheritDoc
+     */
+    public function configureOptions (OptionsResolver $resolver) : void
+    {
+        $resolver
+            ->setDefined(["row_attr"])
+            ->setAllowedTypes("row_attr", "array")
+            ->setDefaults([
+                "row_attr" => [],
+            ]);
+    }
+    /**
+     * @inheritDoc
+     */
+    public static function getExtendedTypes () : array
+    {
+        return [
+            FormType::class,
+        ];
+    }
+}
