8c5f33d2cf9e50ec509fe92a00a1e28a8e3b9bd2
Jannik Zschiesche <jz@becklyn.com>
2019-07-29 11:01:51 +0200

    Add paginator

--- src/Pagination/Pagination.php
+++ src/Pagination/Pagination.php
@@ -22,7 +22,7 @@ class Pagination
     /**
      * @var int
      */
-    private $itemsPerPage;
+    private $perPage;
 
 
     /**
@@ -34,11 +34,11 @@ class Pagination
     /**
      * @param int $currentPage
      * @param int $numberOfItems
-     * @param int $itemsPerPage
+     * @param int $perPage
      */
-    public function __construct (int $currentPage, int $numberOfItems, int $itemsPerPage = 50)
+    public function __construct (int $currentPage, int $numberOfItems, int $perPage = 50)
     {
-        if ($itemsPerPage <= 0)
+        if ($perPage <= 0)
         {
             throw new \InvalidArgumentException("Pagination can only be created for at least 1 item per page.");
         }
@@ -50,12 +50,25 @@ class Pagination
 
         $this->currentPage = $currentPage;
         $this->numberOfItems = $numberOfItems;
-        $this->itemsPerPage = $itemsPerPage;
-        $this->maxPage = (int) \max(1, (int) \ceil($numberOfItems / $itemsPerPage));
+        $this->perPage = $perPage;
+        $this->maxPage = (int) \max(1, (int) \ceil($numberOfItems / $perPage));
+
+        // sanitized current page
+        if ($currentPage < 1)
+        {
+            $this->currentPage = 1;
+        }
+        elseif ($currentPage > $this->maxPage)
+        {
+            $this->currentPage = $this->maxPage;
+        }
     }
 
 
+
     /**
+     * Returns the sanitized current page.
+     *
      * @return int
      */
     public function getCurrentPage () : int
@@ -83,11 +96,11 @@ class Pagination
 
 
     /**
-     * @return bool
+     * @return int
      */
-    public function isValidCurrentPage () : bool
+    public function getPerPage () : int
     {
-        return 1 <= $this->currentPage && $this->currentPage <= $this->maxPage;
+        return $this->perPage;
     }
 
 
@@ -124,7 +137,18 @@ class Pagination
             "max" => $this->maxPage,
             "next" => $this->getNextPage(),
             "prev" => $this->getPreviousPage(),
-            "valid" => $this->isValidCurrentPage(),
+            "perPage" => $this->perPage,
         ];
+    }
+
+
+    /**
+     * @param int $numberOfItems
+     *
+     * @return Pagination
+     */
+    public function withNumberOfItems (int $numberOfItems) : self
+    {
+        return new self($this->currentPage, $numberOfItems, $this->perPage);
     }
 }
---
+++ src/Pagination/Paginator.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace Becklyn\RadBundle\Pagination;
+
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\Tools\Pagination\Paginator as DoctrinePaginator;
+
+class Paginator
+{
+    /**
+     * Returns paginated content.
+     *
+     * @param QueryBuilder $queryBuilder The query builder to retrieve the entities
+     * @param Pagination   $pagination   The pagination object
+     *
+     * @return PaginatedList
+     */
+    public function paginate (QueryBuilder $queryBuilder, Pagination $pagination) : PaginatedList
+    {
+        $totalNumberOfItems = \count(new DoctrinePaginator($queryBuilder->getQuery()));
+        $newPagination = $pagination->withNumberOfItems($totalNumberOfItems);
+
+        if ($totalNumberOfItems > 0)
+        {
+            $queryBuilder
+                ->setFirstResult($newPagination->getCurrentPage() * $newPagination->getPerPage())
+                ->setMaxResults($pagination->getPerPage());
+            $list = \iterator_to_array(new DoctrinePaginator($queryBuilder->getQuery()));
+        }
+        else
+        {
+            $list = [];
+        }
+
+        return new PaginatedList($list, $pagination);
+    }
+}
