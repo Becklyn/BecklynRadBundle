c2dfa22730a700342824dec8e217db98c6738472
Jannik Zschiesche <jz@becklyn.com>
2019-10-30 21:06:26 +0100

    Make model methods fluent

--- src/Model/ModelInterface.php
+++ src/Model/ModelInterface.php
@@ -11,28 +11,32 @@ interface ModelInterface
      * Marks the entity for adding.
      *
      * @param object $entity
+     * @return $this
      */
-    public function add (object $entity) : void;
+    public function add (object $entity);
 
 
     /**
      * Updates the given entity.
      *
      * @param object $entity
+     * @return $this
      */
-    public function update (object $entity) : void;
+    public function update (object $entity);
 
 
     /**
      * Marks the entity for removal.
      *
      * @param object $entity
+     * @return $this
      */
-    public function remove (object $entity) : void;
+    public function remove (object $entity);
 
 
     /**
      * Flushes the entity changes to the database.
+     * @return $this
      */
-    public function flush () : void;
+    public function flush ();
 }
